bfb0928024507215ad6438ce58a5aa50255da5df
Nicolas Amabile <amabile.nicolas@gmail.com>
2023-10-12 02:41:41 -0300

    feature: Calculator tests

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -5,7 +5,35 @@ describe('Calculator', () => {
     cy.visit('/')
   })
 
-  it('Opens the search page', () => {
-    cy.get('textarea[type^="search"').should('be.visible')
+  describe('Visibility', () => {
+    it('Opens the search page', () => {
+      cy.get('textarea[type^="search"').should('be.visible')
+    })
+
+    it('Shows the calculator', () => {
+      cy.searchCalculator()
+    })
+  })
+
+  describe('AC/CE button', () => {
+    it('Deletes last digit when pressing CE button', () => {
+      let operation = '2 + 2'
+      cy.openCalculator()
+      cy.enterOperation(operation)
+      while (operation.length) {
+        cy.checkDisplayValue(operation)
+        cy.clickCalculatorButton('CE')
+        operation = operation.replace(/ /g, '').slice(0, -1); // Remove last char
+      }
+      cy.checkDisplayValue(0)
+    })
+
+    it('Resets the calculator after an operation', () => {
+      cy.openCalculator()
+      cy.executeOperation('2 + 2')
+      cy.checkDisplayValue(4)
+      cy.clickCalculatorButton('AC')
+      cy.checkDisplayValue(0)
+    })
   })
 })
---
+++ cypress/e2e/operations.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+import allOperations from '../fixtures/operations.json'
+const { _ } = Cypress // Local reference to lodash
+
+// Include/exclude desired methods for performing operations
+const executionTypes = {
+  'Typing operation': cy.typeOperation, // Executes an entire operation at once pressing {enter} at the end
+  'Pressing calculator buttons': cy.executeOperation, // Uses the calculator buttons for each keystroke
+}
+
+// Include/exclude operations for test run
+const selectedOperationsForTesting = [
+  'addition',
+  'subtraction',
+  'multiplication',
+  'division',
+  'mixed',
+]
+
+const operations = _.pick(allOperations, selectedOperationsForTesting)
+
+describe('Operations', () => {
+  beforeEach(() => {
+    cy.openCalculator()
+  })
+
+  _.forEach(executionTypes, (runOperation, executionType) => {
+    describe(executionType, () => {
+      _.forEach(operations, (calculations, name) => {
+        describe(_.capitalize(name), () => {
+          calculations.forEach(([calculation, result]) => {
+            it(`${executionType}: ${calculation} = ${result}`, () => {
+              runOperation(calculation)
+              cy.checkDisplayValue(result)
+            })
+          })
+        })
+      })
+    })
+  })
+})
