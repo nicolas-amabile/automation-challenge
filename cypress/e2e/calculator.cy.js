ad2c0a5fa0a991a256b723617ba74843246b1fe8
Nicolas Amabile <amabile.nicolas@gmail.com>
2023-10-11 22:30:15 -0300

    feature: Github workflow

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
 
 describe('Calculator', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Opens the search page', () => {
+    cy.get('textarea[type^="search"').should('be.visible')
+  })
 })
